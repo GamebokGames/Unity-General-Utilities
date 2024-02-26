1edc04cd0fe6fa4b20ca0f2efae685d1031d27d1
Josh <josh@macawls.dev>
2024-02-26 13:04:23 +0200

    init

---
+++ Editor/OpenPersistantDataPath.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Superplay.GeneralUtilities.Editor
+{
+    public static class OpenPersistentDataPath
+    {
+        [MenuItem("Superplay/Utility/Open Persistent Data Path", priority = 0)]
+        public static void Perform()
+        {
+            EditorUtility.RevealInFinder(Application.persistentDataPath);
+        }
+    }
+}
---
+++ Editor/OpenTerminal.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics;
+using UnityEditor;
+using UnityEngine;
+
+namespace Superplay.GeneralUtilities.Editor
+{
+    public static class OpenTerminal
+    {
+        private static readonly string ProjectPath = Application.dataPath + "/..";
+    
+#if UNITY_EDITOR_WIN
+        private const string WindowsTerminalPath = "wt";
+    
+        [MenuItem("Superplay/Utility/Open Terminal/Windows Terminal", priority = 0)]
+        private static void OpenWindowsTerminal()
+        {
+            Process.Start(WindowsTerminalPath, $@"-d ""{ProjectPath}""");
+        }
+
+        [MenuItem("Superplay/Utility/Open Terminal/Command Prompt", priority = 1)]
+        private static void OpenCmd()
+        {
+            Process.Start("cmd.exe", $@"/K cd /d ""{ProjectPath}""");
+        }
+#endif
+    }
+}
+
+
